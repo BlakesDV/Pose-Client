db9fbfcc8e064eba70997d1d49e603d19770452e
BlakesDV <dmunoz@cocoschool.mx>
2025-04-10 00:06:44 -0600

    Arrow controller implementation
    Structure for the arrow controller script

--- Pose MicroGame/Assets/Pose/Code/Arrows.cs
+++ Pose MicroGame/Assets/Pose/Code/Arrows.cs
@@ -1,16 +1,60 @@
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.InputSystem;
+using System.Collections;
 
 public class Arrows : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    #region Runtime Variables
+
+    public Sprite inactiveSprite;
+    public Sprite warningSprite;
+    public Sprite activeSprite;
+    private SpriteRenderer spriteRenderer;
+    private InputAction action;
+    private bool arrowInput;
+    private State actualState = State.Inactive;
+
+    #endregion
+
+    #region Enums
+
+    private enum State { Inactive, Warning, Active };
+
+    #endregion
+
+    private void Start()
     {
-        
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        spriteRenderer.sprite = inactiveSprite;
+
+        StartCoroutine(StateMachine());
     }
 
-    // Update is called once per frame
-    void Update()
+    IEnumerator StateMachine()
     {
-        
+        yield return new WaitForSeconds(2f);
+        actualState = State.Warning;
+        spriteRenderer.sprite = warningSprite;
+
+        yield return new WaitForSeconds(2f);
+        actualState = State.Active;
+        spriteRenderer.sprite = activeSprite;
+
+        //Get input action? or on player call to check state? idk 
+    }
+
+    private void Update()
+    {
+        if (actualState == State.Active && !arrowInput) 
+        { 
+            Vector2 movement = action.ReadValue<Vector2>();
+            if (movement != Vector2.zero)
+            {
+                arrowInput = true;
+                //TODO: cambiar sprites
+            }
+        }
     }
 }
---
+++ Pose MicroGame/Assets/Pose/Code/GameMngr.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.InputSystem;
+using NUnit.Framework;
+
+public class GameMngr : MonoBehaviour
+{
+    
+}
